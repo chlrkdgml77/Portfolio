2b4a0bbdeea4b849a7bdf622f7550d63d0424e3c
김영훈 <joseph0926@naver.com>
2022-08-18 10:44:53 +0900

    2022.08.18

--- ver01/src/pages/About/About.js
+++ ver01/src/pages/About/About.js
@@ -1,9 +1,78 @@
-import React from 'react'
+import React, { useState } from "react";
+
+import Modal from "../../UI/Modal";
+
+import styles from "./About.module.css";
+import { FaArrowAltCircleRight } from "react-icons/fa";
+import Img from "../../img/messi_01.png"
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+  const [modalShow, setModalShow] = useState(false);
+  const modalShowHandler = () => {
+    setModalShow(true);
+  };
+  const modalHideHandler = () => {
+    setModalShow(false);
+  };
 
-export default About
+  return (
+    <div className={styles.container}>
+      <img src={Img}></img>
+      <ul className={styles.lists}>
+        <li className={styles.list}>
+          {modalShow && (
+            <Modal onClose={modalHideHandler}>
+              <h3>Introduce</h3>
+              <p>
+                피고 피가 위하여서, 위하여서. 되는 그들의 피어나는 싶이 길을 것이다. 온갖 품으며, 갑 풀이 못할 아름다우냐? 인생의 있는 이상 <br></br>
+                우리의 그와 내는 되려니와, 우리 봄날의 봄바람이다. 온갖 유소년에게서 위하여 눈에 피어나기 우리 안고, 있으랴? 이상은 위하여 <br></br>
+                같이, 예수는 위하여서. 것은 같은 얼음에 때문이다. 쓸쓸한 피에 어디 가장 그들은 모래뿐일 소담스러운 심장의 때문이다. 되는 <br></br>
+                노년에게서 얼음과 무엇을 그들의 길을 청춘의 사막이다. 목숨을 봄바람을 가슴이 구하지 것이다.
+              </p>
+            </Modal>
+          )}
+          <div className={styles.title} onClick={modalShowHandler}>
+            <h2>자기소개</h2>
+            <FaArrowAltCircleRight></FaArrowAltCircleRight>
+          </div>
+        </li>
+        <li className={styles.list}>
+          {modalShow && (
+            <Modal onClose={modalHideHandler}>
+              <h3>Introduce</h3>
+              <p>
+                피고 피가 위하여서, 위하여서. 되는 그들의 피어나는 싶이 길을 것이다. 온갖 품으며, 갑 풀이 못할 아름다우냐? 인생의 있는 이상 <br></br>
+                우리의 그와 내는 되려니와, 우리 봄날의 봄바람이다. 온갖 유소년에게서 위하여 눈에 피어나기 우리 안고, 있으랴? 이상은 위하여 <br></br>
+                같이, 예수는 위하여서. 것은 같은 얼음에 때문이다. 쓸쓸한 피에 어디 가장 그들은 모래뿐일 소담스러운 심장의 때문이다. 되는 <br></br>
+                노년에게서 얼음과 무엇을 그들의 길을 청춘의 사막이다. 목숨을 봄바람을 가슴이 구하지 것이다.
+              </p>
+            </Modal>
+          )}
+          <div className={styles.title} onClick={modalShowHandler}>
+            <h2>장단점</h2>
+            <FaArrowAltCircleRight></FaArrowAltCircleRight>
+          </div>
+        </li>
+        <li className={styles.list}>
+          {modalShow && (
+            <Modal onClose={modalHideHandler}>
+              <h3>Introduce</h3>
+              <p>
+                피고 피가 위하여서, 위하여서. 되는 그들의 피어나는 싶이 길을 것이다. 온갖 품으며, 갑 풀이 못할 아름다우냐? 인생의 있는 이상 <br></br>
+                우리의 그와 내는 되려니와, 우리 봄날의 봄바람이다. 온갖 유소년에게서 위하여 눈에 피어나기 우리 안고, 있으랴? 이상은 위하여 <br></br>
+                같이, 예수는 위하여서. 것은 같은 얼음에 때문이다. 쓸쓸한 피에 어디 가장 그들은 모래뿐일 소담스러운 심장의 때문이다. 되는 <br></br>
+                노년에게서 얼음과 무엇을 그들의 길을 청춘의 사막이다. 목숨을 봄바람을 가슴이 구하지 것이다.
+              </p>
+            </Modal>
+          )}
+          <div className={styles.title} onClick={modalShowHandler}>
+            <h2>목표 및 태도</h2>
+            <FaArrowAltCircleRight></FaArrowAltCircleRight>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default About;
--- ver01/src/pages/Projects/Projects.js
+++ ver01/src/pages/Projects/Projects.js
@@ -1,9 +1,75 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+
+import styles from "./Projects.module.css";
 
 const Projects = () => {
   return (
-    <div>Projects</div>
-  )
-}
+    <div className={styles.bg}>
+      <div className={styles.container}>
+        <Link to="/projects/m1">
+          <div className={styles.card}>
+            <h3 className={styles.title}>Portfolio Project</h3>
+            <div className={styles.bar}>
+              <div className={styles.emptybar}></div>
+              <div className={styles.filledbar}></div>
+            </div>
+            <div className={styles.circle}>
+              <a href="#" className={styles.a}>
+                More Details
+                <div className={styles.wave}></div>
+              </a>
+            </div>
+          </div>
+        </Link>
+        <Link to="/projects/m2">
+          <div className={styles.card}>
+            <h3 className={styles.title}>Toy Project_<br></br>TransferMarket</h3>
+            <div className={styles.bar}>
+              <div className={styles.emptybar}></div>
+              <div className={styles.filledbar}></div>
+            </div>
+            <div className={styles.circle}>
+              <a href="#" className={styles.a}>
+                More Details
+                <div className={styles.wave}></div>
+              </a>
+            </div>
+          </div>
+        </Link>
+        <Link to="/projects/m3">
+          <div className={styles.card}>
+            <h3 className={styles.title}>Toy Project_<br></br>E-commerce</h3>
+            <div className={styles.bar}>
+              <div className={styles.emptybar}></div>
+              <div className={styles.filledbar}></div>
+            </div>
+            <div className={styles.circle}>
+              <a href="#" className={styles.a}>
+                More Details
+                <div className={styles.wave}></div>
+              </a>
+            </div>
+          </div>
+        </Link>
+        <Link to="/projects/m4">
+          <div className={styles.card}>
+            <h3 className={styles.title}>Dummy</h3>
+            <div className={styles.bar}>
+              <div className={styles.emptybar}></div>
+              <div className={styles.filledbar}></div>
+            </div>
+            <div className={styles.circle}>
+              <a href="#" className={styles.a}>
+                More Details
+                <div className={styles.wave}></div>
+              </a>
+            </div>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default Projects
+export default Projects;
